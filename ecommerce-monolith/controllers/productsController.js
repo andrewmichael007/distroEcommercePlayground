6a9493860e951f2ab78866940834f5378d4f20bf
Michael Andrew <adjeimitchel03@gmail.com>
2025-05-28 22:12:39 +0000

    creating a productsController

--- ecommerce-monolith/controllers/productsController.js
+++ ecommerce-monolith/controllers/productsController.js
@@ -1,1 +1,21 @@
+//making an array of  dummy product
+let dummyProducts = [
+  { id: 1, name: 'laptop' , price: '1200'},
+  { id: 2, name: 'monitor' , price: '500' },
+  { id: 3, name: 'keyboard' , price: '200' },
+  { id: 4, name: 'phone' , price: '800' }
+];
 
+//getting user products
+exports.getProducts = (req, res) => { 
+  res.json({products: , dummyProducts});
+};
+
+
+exports.addProducts = (req, res) => { 
+  const { name, price } = req.body;
+
+  
+
+
+}
