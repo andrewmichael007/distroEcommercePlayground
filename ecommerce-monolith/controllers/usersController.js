a692d90543e543d3add523bbde42029015d8c0b5
Michael Andrew <adjeimitchel03@gmail.com>
2025-05-28 20:22:14 +0000

    created a route to register user

--- ecommerce-monolith/controllers/usersController.js
+++ ecommerce-monolith/controllers/usersController.js
@@ -1,1 +1,13 @@
+//controller controlls the logic
+//the flow: 
+exports.registerUser = (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email  || !password) {
+    return res.status(400).json({message: "Missing fields"});
+  }
+  else {
+    const user = {id: Date.now(), name, email };
+    return res.status(201).json ({message: "User registered", user});
+};
 
