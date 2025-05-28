e4a2c6cab9b99c0d03285881dac2a09d435d97d6
Michael Andrew <adjeimitchel03@gmail.com>
2025-05-28 21:48:10 +0000

    added a router for the products routes

--- ecommerce-monolith/routes/products.js
+++ ecommerce-monolith/routes/products.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.router();
 
+const { getProducts, createProducts } = require("../controllers/productsController");
+
+router.get("/", getProducts);
+router.post("/", createProducts);
+
+module.exports = router;
