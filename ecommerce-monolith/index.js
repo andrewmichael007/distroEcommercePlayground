687d472412166ba61904310ee38aa96e26dcda2b
Michael Andrew <adjeimitchel03@gmail.com>
2025-05-28 19:45:36 +0000

    created index.js file

--- ecommerce-monolith/index.js
+++ ecommerce-monolith/index.js
@@ -1,7 +1,36 @@
 const express = require("express");
+
+// allows external clients to access the API.
 const cors = require("cors");
+
+//logs HTTP requests.
 const morgan = require("morgan");
 require("dotenv).config();
 
+//storing routes
+const userRoutes = require("./routes/users");
+const productRoutes = require("./routes/products");
+const orderRoutes = require("./routes/orders");
+
+// using various frameworks
 const app = express();
-app.use(express(json));
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(morgan());
+
+ //parses incoming JSON requests
+app.use(express.json());
+
+// Routes
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+
+// Fallback route
+app.use((req, res) => res.status(404).send('Route not found'));
+
+//listening 
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
