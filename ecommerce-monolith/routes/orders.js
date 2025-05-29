58633439bf711006ce011cfc0221fe23328de38f
Michael Andrew <adjeimitchel03@gmail.com>
2025-05-29 10:32:59 +0000

    added the snippet for routing orders

--- ecommerce-monolith/routes/orders.js
+++ ecommerce-monolith/routes/orders.js
@@ -1,1 +1,25 @@
+const express = require("express");
+
+const router = express.Router();
+
+// we put whatever we want to do (functions) in the controller here.
+// for the mean time, we want to just get orders and make orders
+// ... so we put get orders and make orders in the bracket
+// .... and require the logic from the orders controller
+const { getOrders, makeOrders } = require("../controllers/ordersController");
+
+//then come and make a route for them over here
+router.get('/', getOrders);
+
+router.get('/', makeOrders);
+
+
+
+
+
+
+
+
+
+
 
