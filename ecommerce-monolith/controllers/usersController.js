acad9dcd0b7022446a4f55733683e07150675212
Michael Andrew <adjeimitchel03@gmail.com>
2025-05-29 14:07:46 +0000

    modifying the code of the user route

--- ecommerce-monolith/controllers/usersController.js
+++ ecommerce-monolith/controllers/usersController.js
@@ -1,13 +1,24 @@
-//controller controlls the logic
-//the flow: 
-exports.registerUser = (req, res) => {
+//CONTROLLING THE USER REGISTRATION LOGIC
+// arrow function for registering user
+const registerUser = (req, res) => {
+  
   const { name, email, password } = req.body;
-
+  
   if (!name || !email  || !password) {
     return res.status(400).json({message: "Missing fields"});
-  }
-  else {
-    const user = {id: Date.now(), name, email };
+  } else {
+    //create a new user
+    const user = {
+      id: Date.now(),
+      name, 
+      email 
+    };
+    // return a message to show that new user is created
     return res.status(201).json ({message: "User registered", user});
 };
 
+
+  //just tryin' my hands on deleting a user
+  const deleteUser =
+//export the function
+module.exports = { registerUser };
