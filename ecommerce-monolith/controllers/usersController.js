4d9a2b5d0f7a9ab8014d8ab5010bdddd47dc2ae5
Michael Andrew <adjeimitchel03@gmail.com>
2025-05-29 16:53:55 +0000

    added  deleting and updating user functionalities

--- ecommerce-monolith/controllers/usersController.js
+++ ecommerce-monolith/controllers/usersController.js
@@ -16,5 +16,37 @@ const registerUser = (req, res) => {
     return res.status(201).json ({message: "User registered", user});
 };
 
+  //THIS IS THE LOGIC FOR DELETING USERS
+  //making an array of deleted users
+let deletedUsers = [];
+  
+//let's say i want to delete a user
+const deleteUser = (req, res) => {
+  
+  const { name, email,  password } = req.body;
+  
+  if (!name || !email || !password){
+    res.status(400).json({ message: "Missing fields" });
+  } else {
+    const deletedUser = {
+      id: Date.now(),
+      name: " ",
+      email: " " 
+    };
+    //adding deleted user to the array  of deleted users
+    deletedUsers.push(deletedUser);
+
+    //send out a json message to show that user is deleted
+    res.status(200).json({ message: "User Deleted" });
+  };
+};
+
+  
+  //updating users
+  const updateUser = (req, res) => {
+    const { name, email, password } = req.body;
+
+    
+  };
 //export the function
-module.exports = { registerUser };
+module.exports = { registerUser, deleteUser };
