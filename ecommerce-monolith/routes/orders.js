b6ac32c7166c509b6e2577ea52cfe76f1e3b4fea
Andy-Techy <adjeimitchel03@gmail.com>
2025-08-13 04:41:12 +0000

    contains orders route

--- ecommerce-monolith/routes/orders.js
+++ ecommerce-monolith/routes/orders.js
@@ -6,12 +6,15 @@ const router = express.Router();
 // for the mean time, we want to just get orders and make orders
 // ... so we put get orders and make orders in the bracket
 // .... and require the logic from the orders controller
-const { placeOrders, getOrders } = require("../controllers/ordersController");
+const { placeOrder } = require("../controllers/ordersController");
 
 //then come and make a route for them over here
-router.get('/', placeOrders);
+router.post('/order', placeOrder );
 
-router.post('/', getOrders);
+// router.post('/', getOrders);
+
+
+module.exports = router;
 
 
 
