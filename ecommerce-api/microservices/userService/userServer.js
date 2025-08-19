5e152bc6566bef2d93604d190fcfdcc2f23de18d
Michael Andrew <adjeimitchel03@gmail.com>
2025-08-19 21:09:27 +0000

    creating the user server

--- ecommerce-api/microservices/userService/userServer.js
+++ ecommerce-api/microservices/userService/userServer.js
@@ -1,1 +1,45 @@
 
+//require statements
+const express = require("express");
+const mongoose = require("mongoose");
+// const uuid = require("uuid");
+// const createBugsRoute = require( "./Routes/createBugsRoute" );
+// const getBugsRoute = require( "./Routes/getBugsRoute" );
+// const updateBugsRoute = require( "./Routes/updateBugsRoute" );
+// const deleteBugsRoute = require( "./Routes/deleteBugsRoute" );
+require("dotenv").config();
+
+//builds a mini server call app
+const app = express();
+
+//understands json format of data
+app.use(express.json());
+
+//server mounting at "/" +  route path = final url "/bug"
+
+//create bugs server mount
+app.use( '/', createBugsRoute);
+
+
+//mongodb connection
+mongoose.connect(process.env.MONGO_URI, {
+    // deprecated in mongoose 6.x
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+})
+.then(() => 
+    console.log("mongodb connected😎")
+)
+.catch(err => 
+    console.error(err)
+);
+
+// taking the port number from the .env file and store in port
+const port = process.env.PORT;
+
+//listening for incoming requests
+app.listen(port, () => {
+    console.log(`server is running on port ${port}!🚀`);
+});
+
