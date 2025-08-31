beb64d6bdfa5f7e3ea21f8d929ed5f920bb0935e
Andy-Techy <adjeimitchel03@gmail.com>
2025-08-31 12:38:53 +0000

    this is the route for server

--- ecommerce-api/microservices/userService/userRoute.js
+++ ecommerce-api/microservices/userService/userRoute.js
@@ -1,12 +1,21 @@
 //require express
 const express = require("express");
 
+
 const router = express.Router();
 
-const { register } = require("./userController.js");
+const { register , validator } = require("./userController.js");
 
 
 //then come and make a route for them over here
-router.post('/register', register ); 
+router.post(
+
+    "/register",
+
+    validator,
+
+    register
+);
 
 module.exports = router;
+
