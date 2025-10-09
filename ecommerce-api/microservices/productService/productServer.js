6d18fda1d966e14571f52fe42ddd40e7cdbd6c9f
Andy-Techy <adjeimitchel03@gmail.com>
2025-10-09 07:10:00 +0000

    this is the product server, contains mongodb connection as well as redis connection too

--- ecommerce-api/microservices/productService/productServer.js
+++ ecommerce-api/microservices/productService/productServer.js
@@ -1,10 +1,16 @@
 
+// connection of mongo db and redis
 //require statements
 const express = require("express");
 const mongoose = require("mongoose");
+import { createClient } from "redis";
 // const createBugsRoute = require( "./Routes/createBugsRoute" );
+const  productRoute = ("./Routes/productRoute");
+
 
 require("dotenv").config();
+
+// import dotenv from "dotenv";
 
 //builds a mini server call app
 const app = express();
@@ -14,9 +20,8 @@ app.use(express.json());
 
 //server mounting at "/" +  route path = final url "/bug"
 
-//create bugs server mount
-// app.use( '/', createBugsRoute);
-
+// create product server mount
+app.use( '/', productRoute);
 
 //mongodb connection
 mongoose.connect(process.env.PRODUCT_SERVICE_MONGO_URI, {
@@ -25,18 +30,35 @@ mongoose.connect(process.env.PRODUCT_SERVICE_MONGO_URI, {
     // useUnifiedTopology: true,
     // useCreateIndex: true,
 })
-.then(() => 
+.then(() =>
     console.log("mongodb connected to user product serviceDb😎")
 )
-.catch(err => 
+.catch(err =>
     console.error(err)
 );
 
 // taking the port number from the .env file and store in port
 const port = process.env.PRODUCT_SERVICE_PORT;
 
+// redis connection
+const redisClient = createClient({
+    url: process.env.REDIS_URL
+});
+
+redisClient.on("error", (err) =>
+    console.log("Redis Client Error", err));
+
+await redisClient.connect();
+
+app.use((req, res, next) => {
+  req.redis = redisClient; // attach redis to req for route use
+  next();
+});
+
 //listening for incoming requests
 app.listen(port, () => {
-    console.log(`server is running on port ${port}!🚀 for the product service`);
+    console.log(`server is running on port ${port}!🚀 for the product service`)
 });
+
+
 
